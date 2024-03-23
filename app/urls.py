b043ee98e8da15294e1e95b9e9c23c28ab36b002
Rafaelkrc <rafaelkrc@hotmail.com>
2024-03-23 20:17:47 +0000

    create custom user

--- app/urls.py
+++ app/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app.employees.views import home
+from app.base.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
